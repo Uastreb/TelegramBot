95c63b4a20719526d3d69725fbf3bdb959ae89f5
Vadim <try.proger.vadim@gmail.com>
2022-12-04 20:35:18 +0300

    Поправил таблицу DailyEarning, сделал первичный составной ключ

---
+++ CryptoExchangeBot/Migrations/20221204172801_ChangeDailyEarningPrimaryKeys.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoExchangeBot.Migrations
+{
+    public partial class ChangeDailyEarningPrimaryKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DailyEarnings",
+                table: "DailyEarnings");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ChatId",
+                table: "DailyEarnings",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DailyEarnings",
+                table: "DailyEarnings",
+                columns: new[] { "ChatId", "DateCreated" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DailyEarnings",
+                table: "DailyEarnings");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ChatId",
+                table: "DailyEarnings",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DailyEarnings",
+                table: "DailyEarnings",
+                column: "ChatId");
+        }
+    }
+}
